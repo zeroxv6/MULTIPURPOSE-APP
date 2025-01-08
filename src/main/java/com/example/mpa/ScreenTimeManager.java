486c389d081004514031abd0a6f6ff7a01eaafb2
Raman Mann <94175362+ipRMNv4@users.noreply.github.com>
2025-01-08 17:40:48 +0530

    Update ScreenTimeManager.java

--- src/main/java/com/example/mpa/ScreenTimeManager.java
+++ src/main/java/com/example/mpa/ScreenTimeManager.java
@@ -4,10 +4,10 @@ import java.sql.*;
 import java.util.concurrent.TimeUnit;
 public class ScreenTimeManager {
 
-    public void updateAppScreenTime(String applicationName, long activeTime) {
+    public void updateAppScreenTime(String applicationName, long activeTime, int userId) {
         String sql = """
-            INSERT INTO screenMonitoring (applicationName, activeTime)
-            VALUES (?, ?)
+            INSERT INTO screenmonitoring (applicationName, activeTime, userId)
+            VALUES (?, ?, ?)
             ON DUPLICATE KEY UPDATE activeTime = activeTime + ?;
         """;
 
@@ -15,52 +15,47 @@ public class ScreenTimeManager {
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, applicationName);
             pstmt.setLong(2, activeTime);
-            pstmt.setLong(3, activeTime);
+            pstmt.setInt(3, userId);
+            pstmt.setLong(4, activeTime);
 
             pstmt.executeUpdate();
-//            System.out.println("Updated screen time for " + applicationName);
         } catch (SQLException e) {
-//            System.out.println("Error: Unable to update screen time.");
             e.printStackTrace();
         }
     }
-    public double printSumTime() {
-        String sql = "SELECT SUM(activeTime) FROM screenMonitoring";
-//        StringBuilder screenSumData = new StringBuilder();
+    public double printSumTime(int userId) {
+        String sql = "SELECT SUM(activeTime) FROM screenmonitoring WHERE userId = ?";
         double screenSumData = 0.0;
         try (Connection conn = DatabaseManager.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery()) {
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, userId);
+            ResultSet rs = pstmt.executeQuery();
 
-            while (rs.next()) {
+            if (rs.next()) {
                 double activeTimeInMillis = rs.getDouble("SUM(activeTime)");
-
-                screenSumData = activeTimeInMillis/86400000;
-//                screenSumData.append(String.format("%s", formattedTime));
+                screenSumData = activeTimeInMillis / 86400000;
             }
         } catch (SQLException e) {
-//            System.out.println("Error: Unable to retrieve screen sum data.");
             e.printStackTrace();
         }
         return screenSumData;
     }
 
-    public String printScreenTime() {
-        String sql = "SELECT * FROM screenMonitoring ORDER BY activeTime DESC";
+    public String printScreenTime(int userId) {
+        String sql = "SELECT * FROM screenmonitoring WHERE userId = ? ORDER BY activeTime DESC";
         StringBuilder screenTimeData = new StringBuilder();
         try (Connection conn = DatabaseManager.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery()) {
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, userId);
+            ResultSet rs = pstmt.executeQuery();
 
             while (rs.next()) {
                 String applicationName = rs.getString("applicationName");
                 long activeTimeInMillis = rs.getLong("activeTime");
                 String formattedTime = conversion(activeTimeInMillis);
-
                 screenTimeData.append(String.format("%s, %s%n", applicationName, formattedTime));
             }
         } catch (SQLException e) {
-//            System.out.println("Error: Unable to retrieve screen time data.");
             e.printStackTrace();
         }
         return screenTimeData.toString();
@@ -73,44 +68,55 @@ public class ScreenTimeManager {
     }
 
 
-    public long getActiveTime() {
-        String sql = "SELECT activeTime FROM screenMonitoring";
-
+    public long getActiveTime(int userId) {
+        String sql = "SELECT activeTime FROM screenmonitoring WHERE userId = ?";
         long activeTime = 0;
         try (Connection conn = DatabaseManager.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery()) {
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, userId);
+            ResultSet rs = pstmt.executeQuery();
 
-            while (rs.next()) {
-                rs.getLong("activeTime");
+            if (rs.next()) {
+                activeTime = rs.getLong("activeTime");
             }
         } catch (SQLException e) {
-//            System.out.println("Error: Unable to retrieve screen time data.");
             e.printStackTrace();
         }
         return activeTime;
     }
 
 
-    public void deleteAppScreenTime(String applicationName) {
-        String sql = "DELETE FROM screenMonitoring WHERE applicationName = ?";
-
+    public void deleteAppScreenTime(String applicationName, int userId) {
+        String sql = "DELETE FROM screenmonitoring WHERE applicationName = ? AND userId = ?";
         try (Connection conn = DatabaseManager.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, applicationName);
-
+            pstmt.setInt(2, userId);
 
             int rowsAffected = pstmt.executeUpdate();
-//            if (rowsAffected > 0) {
-//                System.out.println("Deleted screen time data for " + applicationName);
-//            } else {
-//                System.out.println("No data found for " + applicationName);
-//            }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Error: Unable to delete screen time data.");
             e.printStackTrace();
         }
+    }
+    public int getUserId(String userName) {
+        String sql = "SELECT userId FROM userdata WHERE login = ?";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, userName);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return rs.getInt("userId");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: Unable to retrieve userId.");
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
 
