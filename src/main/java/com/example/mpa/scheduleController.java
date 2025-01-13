4fb3476cce76209b74f24e0823c9da57ccfb17d2
Raman Mann <94175362+0xV6@users.noreply.github.com>
2025-01-13 15:00:03 +0530

    Update scheduleController.java

--- src/main/java/com/example/mpa/scheduleController.java
+++ src/main/java/com/example/mpa/scheduleController.java
@@ -3,7 +3,13 @@ package com.example.mpa;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -43,6 +49,17 @@ public class scheduleController implements Initializable {
     @FXML
     private TextField titleBox;
 
+    @FXML
+    private Button schSave;
+
+    @FXML
+    private TextArea schTopics;
+
+    String filePath = "src/main/resources/data/user.json";
+    ScreenTimeManager manager = new ScreenTimeManager();
+    int userID = manager.getUserId(readUserFromJSON(filePath));
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         for (int i = 1; i <= 12; i++) {
@@ -67,7 +84,41 @@ public class scheduleController implements Initializable {
         date1.setEditable(false);
         date2.setEditable(false);
 
+        schSave.setOnAction(event -> {
+            scheduleManager manager = new scheduleManager();
+            manager.addSchedule(userID,getTitle(),getTimeFrom(),getTimeTo(),getDateFrom(),getDateTo(),getTopics());
+        });
 
+
+    }
+    public static String readUserFromJSON(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            StringBuilder jsonContent = new StringBuilder();
+            String line;
+            boolean jsonStarted = false;
+
+            while ((line = br.readLine()) != null) {
+                if (line.trim().startsWith("{")) {
+                    jsonStarted = true;
+                }
+                if (jsonStarted) {
+                    jsonContent.append(line);
+                }
+            }
+
+            JSONObject jsonObject = new JSONObject(jsonContent.toString());
+            JSONArray userData = jsonObject.getJSONArray("userData");
+
+            if (userData.length() > 0) {
+                JSONObject userObject = userData.getJSONObject(0);
+                return userObject.getString("user");
+            }
+
+        } catch (IOException | JSONException e) {
+            System.out.println("Error reading JSON file: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public String getTitle(){
@@ -75,27 +126,51 @@ public class scheduleController implements Initializable {
         return schTitle;
     }
 
-    public String getTimeFrom(){
-        int fromH = (int) time1.getValue();
-        int fromM = (int) time11.getValue();
+    public String getTimeFrom() {
 
-        String timeFrom = String.format("%02d : %02d", fromH, fromM);
+        int fromHour;
+        String fromM = (String) time11.getValue();
+
+        if(B2.isSelected()){
+            fromHour = Integer.parseInt(time1.getValue().toString()) + 12;
+        }
+        else{
+            fromHour = Integer.parseInt(time1.getValue().toString());
+        }
+        System.out.println(fromHour);
+
+        String timeFrom = String.format("%02d:%02d", fromHour, Integer.parseInt(fromM));
+        System.out.println(timeFrom);
         return timeFrom;
     }
-    public String getTimeTo(){
-        int toH = (int) time2.getValue();
-        int toM = (int) time21.getValue();
 
-        String timeTo = String.format("%02d : %02d", toH, toM);
-        return timeTo;
+    public String getTimeTo() {
+        int toHour;
+        String toM = (String) time21.getValue();
+
+        if(B2.isSelected()){
+            toHour = Integer.parseInt(time2.getValue().toString()) + 12;
+        }
+        else{
+            toHour = Integer.parseInt(time2.getValue().toString());
+        }
+        System.out.println(toHour);
+
+        String timeFrom = String.format("%02d:%02d", toHour, Integer.parseInt(toM));
+        System.out.println(timeFrom);
+        return timeFrom;
     }
-    public String getDateFrom(){
-        LocalDate fromD = date1.getValue();
-        return fromD.toString();
+    public LocalDate getDateFrom(){
+        String fromD = date1.getValue().toString();
+        return LocalDate.parse(fromD);
     }
-    public String getDateTo(){
-        LocalDate toD = date2.getValue();
-        return toD.toString();
+    public LocalDate getDateTo(){
+        String toD = date2.getValue().toString();
+        return LocalDate.parse(toD);
+    }
+    public String getTopics(){
+        String topics = schTopics.getText();
+        return topics;
     }
 }
 
